c5a15b9cef6efae86b4e8a890eba205a980fc068
PatrickSpace <jplosta01@gmail.com>
2019-09-13 11:05:53 -0500

    Actualizar el sistema solar

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,16 @@ function hola()
     alert('hola');
 }
 
+function expandir_proyectos()
+{
+    $('.tierra#ccv,#ccontacto').hide(1.5, function () {
+        $('#proyects').addClass('objactive');
+        
+    });
+}
+
+$(document).ready(function () {
+
+
+});
+
